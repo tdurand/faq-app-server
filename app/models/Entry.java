4119c010807a2549d18f8aba353a8dc177b4e11d
Thibault Durand <tibo.durand@gmail.com>
2012-09-26 17:58:17 +0200

    WIP: Supress MaxSize annotation

--- app/models/Entry.java
+++ app/models/Entry.java
@@ -18,27 +18,25 @@ public class Entry extends Model {
     public Category category;
 	
     public String title_fr;
-	@MaxSize(10000)
+	
     public String desc_fr;
 	
 	public String title_en;
-    @MaxSize(10000)
+    
     public String desc_en;
     
     public String title_es;
-    @MaxSize(10000)
+    
     public String desc_es;
     
     public String title_it;
-    @MaxSize(10000)
+    
     public String desc_it;
     
     public String title_de;
-    @MaxSize(10000)
     public String desc_de;
     
     public String title_pt;
-    @MaxSize(10000)
     public String desc_pt;
 
     @Override
